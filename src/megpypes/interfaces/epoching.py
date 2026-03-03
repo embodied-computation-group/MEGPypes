c296bd57b0482e14db10f11b5bd9b193f02e4315
ElisiusKLP <elisius@live.dk>
2026-03-03 13:51:47 +0100

    began writing epoching

--- src/megpypes/interfaces/epoching.py
+++ src/megpypes/interfaces/epoching.py
@@ -3,7 +3,7 @@
 """
 import os
 import mne
-import autoreject
+from autoreject import AutoReject
 from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec, TraitedSpec, traits
 import logging
 
@@ -24,7 +24,7 @@ class EpochingInputSpec(BaseInterfaceInputSpec):
     
     
     # output
-    out_file = traits.Str("epoched_raw.fif", usedefault=True, desc="Output filename")
+    out_file = traits.Str("epoched_raw-epo.fif", usedefault=True, desc="Output filename")
 
 class EpochingOutputSpec(TraitedSpec):
     out_file = traits.File(exists=True, desc="Epoched MEG file")
@@ -49,9 +49,49 @@ class Epoching(BaseInterface):
         events = mne.find_events(raw)
 
         mask = events[:, 2] == self.inputs.event_id[self.inputs.event_id]
+        selected_events = events[mask]
+
+        if len(selected_events) == 0:
+            raise RuntimeError(
+                f"No events found for trigger {self.inputs.event_code}"
+            )
         
+        epochs = mne.Epochs(
+            raw,
+            events=selected_events,
+            tmin=self.inputs.event_tmin,
+            tmax=self.inputs.event_tmax,
+            baseline=None
+        )
+
 
         # 2. Autoreject-based epoch rejection
         if self.inputs.enable_autoreject:
-            logger.info("Applying autoreject-based epoch rejection")
+            logger.info("Running autoreject on epochs")
+            # TODO: picks should be created based on channel types present in the data 
+            # AutoReject can only handle on type at a time
+            picks = mne.pick_types(epochs.info, meg=True, eeg=False, eog=False, stim=False)
+            ar = AutoReject(
+                n_interpolates=[1, 4, 32], # TODO: What should these values be?
+                consensus=[0.5, 1], # TODO: What should these values be?
+                picks=picks,
+                thresh_method="random_search",
+                random_state=893
+            )
+            # TODO: Save the ar object for later QC of rejected epochs and channels
+            # we can use the included ar.get_reject_log() function 
+            ar_epochs = ar.transform(epochs)
 
+        # Save epoched data
+        logger.info(f"OUT FILE PATH: {self.inputs.out_file}")
+        out_path = os.path.abspath(self.inputs.out_file)
+        ar_epochs.save(out_path, overwrite=True)
+        logger.info(f"Saved: {out_path}")
+
+        runtime.returncode = 0
+        return runtime
+    
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs["out_file"] = os.path.abspath(self.inputs.out_file)
+        return outputs
